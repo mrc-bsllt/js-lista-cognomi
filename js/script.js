fd46cdb40acebd760f222e771e6fde1589f2b38b
MarcoB <mrc.bsllt@gmail.com>
2020-11-16 17:57:50 +0100

    aggiunto alert nel caso il cognome fosse già presente

--- js/script.js
+++ js/script.js
@@ -9,19 +9,27 @@ var listaCognomi = ["Rossi", "Busellato", "Fogo", "Dal Pozzolo", "Boschiero", "A
 var inputUtente = prompt("Inserisci il tuo cognome");
 var cognome = inputUtente = inputUtente[0].toUpperCase() + inputUtente.slice(1);
 
-listaCognomi.push(cognome);
-
-// faccio in modo che la lista dei cognomi venga ordinata alfabeticamente
-listaCognomi.sort();
-
-var surname_list = document.getElementById("surname_list");
-
-// scrivo nella pagina la lista dei cognomi nell'ordine dell'Array
-for (var i = 0; i < listaCognomi.length; i++) {
-  surname_list.innerHTML += "<li>" + listaCognomi[i] + "</li>";
+var condizione = false;
+for(var i = 0; i < listaCognomi.length; i++) {
+  if (cognome == listaCognomi[i]) {
+    condizione = true;
+  }
 }
 
-// aggiungo la posizione dell'Utente all'interno della lista
-var posizioneUtenteNuovo = (listaCognomi.indexOf(cognome)) + 1;
+if (condizione == true) {
+  alert("Il cognome è già presente nella lista")
+} else {
+  // inserisco il cognome nella lista e ordino tutto in ordine alfabetico
+  listaCognomi.push(cognome);
+  listaCognomi.sort();
+  // prendo l'elemento dove poi inserirò tutta la lista
+  var surname_list = document.getElementById("surname_list");
+  // scrivo nella pagina la lista dei cognomi nell'ordine dell'Array
+  for (var i = 0; i < listaCognomi.length; i++) {
+    surname_list.innerHTML += "<li>" + listaCognomi[i] + "</li>";
+  }
+  // aggiungo la posizione dell'Utente all'interno della lista
+  var posizioneUtenteNuovo = (listaCognomi.indexOf(cognome)) + 1;
 
-var position = document.getElementById("position").innerHTML = "Sei il numero " + "<span class='red'>" + posizioneUtenteNuovo + "</span>" + " della lista.";
+  var position = document.getElementById("position").innerHTML = "Sei il numero " + "<span class='red'>" + posizioneUtenteNuovo + "</span>" + " della lista.";
+}
