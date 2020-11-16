3f12ddfba4d4cebb9fe8c19166d77127600939f7
MarcoB <mrc.bsllt@gmail.com>
2020-11-16 14:20:10 +0100

    creato una lista ordinata alfabeticamente di cognomi

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Lista Cognomi
+// Chiedere all’utente il cognome,
+// inserirlo in un array con altri cognomi (Es: 'Pesce', ‘Castenetto’, 'Passaro', 'Grimaldi', ‘Rossi’, ‘Verdi’, ‘Bianchi’).
+// Stampa la lista ordinata alfabeticamente.
+// Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova.
+
+var listaCognomi = ["Rossi", "Busellato", "Fogo", "Dal Pozzolo", "Boschiero", "Aiello", "Dal Santo", "Oro", "Di Barbora", "Sartori"];
+
+var cognomeUtente = prompt("Inserisci il tuo cognome");
+listaCognomi.push(cognomeUtente);
+
+// faccio in modo che la lista dei cognomi venga ordinata alfabeticamente
+listaCognomi.sort();
+
+var listaCognomiScritta = document.getElementById("surname_list");
+
+// scrivo nella pagina la lista dei cognomi nell'ordine dell'Array
+for (var i = 0; i < listaCognomi.length; i++) {
+  listaCognomiScritta.innerHTML += "<li>" + listaCognomi[i] + "</li>";
+}
